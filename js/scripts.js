2f5ae950354b215a01ea634b800c3aef3fe47940
Macsuel Dias <66310986+macsueldias@users.noreply.github.com>
2020-07-23 21:06:01 -0300

    Calculadora
    JS, operações

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,81 @@ class Calculator {
         this.resultValue = document.querySelector('#result-number')
         this.reset = 0
     }
+
+    clearValues() {
+        this.upperValue.textContent = '0'
+        this.resultValue.textContent = '0'
+    }
+    checkLastDigit(input, upperValue, reg) {
+        if ((
+            !reg.test(input) &&
+            !reg.test(upperValue.substr(upperValue.length - 1))
+        )) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    // resolve a operação
+    resolution() {
+
+        //explode uma string em um array
+        let upperValueArray = (this.upperValue.textContent).split(' ')
+
+        // resultado da operação
+        let result = 0
+        
+        for(let i = 0; i <= upperValueArray.length; i++){
+            let actualItem = upperValueArray[i]
+
+            if(actualItem == '+') {
+                result = parseFloat(upperValueArray[i - 1]) + parseFloat(upperValueArray[i + 1])
+            }if(actualItem == 'x') {
+                result = parseFloat(upperValueArray[i - 1]) * parseFloat(upperValueArray[i + 1])
+            }if(actualItem = '/') {
+                result = parseFloat(upperValueArray[i - 1]) / parseFloat(upperValueArray[i + i])
+            }if(actualItem = '-') {
+                result = parseFloat(upperValueArray[i - 1]) - parseFloat(upperValueArray[i + 1])
+            }
+        }
+        this.upperValue.textContent = result
+        this.resultValue.textContent = result
+    }
+
     btnPress() {
-        console.log(this)
+        let input = this.textContent
+        let upperValue = calc.upperValue.textContent
+        // verificar se tem só números
+        var reg = new RegExp('^\\d+$')
+
+        // ativa método de limpar o display
+        if (input == 'AC') {
+
+            calc.clearValues()
+
+        }else if(input == '=') {
+
+            calc.resolution()
+
+        } else {
+
+            // checa se precisa adicionar ou não
+            if (calc.checkLastDigit(input, upperValue, reg)) {
+                return false
+            }
+
+            // adiciona espaços aos operadores
+            if (!reg.test(input)) {
+                input = ` ${input} `
+            }
+
+            if (upperValue == '0') {
+                calc.upperValue.textContent = input
+            } else {
+                calc.upperValue.textContent += input
+            }
+        }
     }
 }
 
@@ -16,8 +89,6 @@ let calc = new Calculator
 let buttons = document.querySelectorAll('.btn')
 
 // map all buttons
-for(let i = 0; buttons.length > i; i++) {
+for (let i = 0; buttons.length > i; i++) {
     buttons[i].addEventListener('click', calc.btnPress)
 }
-
-console.log(buttons)
